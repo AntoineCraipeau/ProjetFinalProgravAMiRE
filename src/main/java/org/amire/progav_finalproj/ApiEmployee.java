b57f74dcb1ce10a5ff3bf250e747e643a279fad9
LeozeCroissant <leo.foulloy@gmail.com>
2023-10-12 11:48:42 +0200

    minor change to ApiEmployee

--- src/main/java/org/amire/progav_finalproj/ApiEmployee.java
+++ src/main/java/org/amire/progav_finalproj/ApiEmployee.java
@@ -5,12 +5,19 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.amire.progav_finalproj.model.EnseignantEntity;
 import org.amire.progav_finalproj.model.UserSessionBean;
+import org.amire.progav_finalproj.model.EnseignantSessionBean;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java.sql.Timestamp;
 
 @Path("/users")
 public class ApiEmployee {
 
     UserSessionBean userSessionBean = new UserSessionBean();
+    EnseignantSessionBean enseignantSessionBean = new EnseignantSessionBean();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -18,5 +25,16 @@ public class ApiEmployee {
         return userSessionBean.getAllUsers().toString();
     }
 
+    @GET
+    @Path("/disponibilites")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Timestamp> getDisponibilites() {
+        List<EnseignantEntity> enseignants = enseignantSessionBean.getAllEnseignants();
 
+        List<Timestamp> dispos = enseignants.stream()
+                .map(EnseignantEntity::getDisponibilites)
+                .collect(Collectors.toList());
+
+        return dispos;
+    }
 }
